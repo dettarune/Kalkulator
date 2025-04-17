1970f1f9ecb3dfef3531d675fea40f7d75ea2dd2
robotganteng09 <arsyafaisyar@gmail.com>
2025-04-17 11:07:26 +0700

    feat[AMF]/login-page

--- app/src/main/java/com/example/kalkulator/MainActivity.java
+++ app/src/main/java/com/example/kalkulator/MainActivity.java
@@ -1,6 +1,11 @@
 package com.example.kalkulator;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,15 +15,35 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    EditText edUsername;
+    EditText edPassword;
+    Button btnLogin;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        edUsername = (EditText) findViewById(R.id.edUsername);
+        edPassword = (EditText) findViewById(R.id.edPassword);
+        btnLogin = (Button) findViewById(R.id.btnLogin);
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                if(edUsername.getText().toString().equals("admin")
+                        && edPassword.getText().toString().equals("admin123")) {
+                    Toast.makeText(MainActivity.this, "Login Succes", Toast.LENGTH_SHORT).show();
+                    Intent intent = new Intent(MainActivity.this, Kalkulator.class);
+                    startActivity(intent);
+                } else {
+                    Toast.makeText(MainActivity.this, "Login Gagal", Toast.LENGTH_SHORT).show();
+                }
+
+            }
         });
+
     }
 }
